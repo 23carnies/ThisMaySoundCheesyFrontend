beb46797283de262aebfa822d7beaac015c3f7ec
RobBloxham <acidwash52@gmail.com>
2020-09-17 14:26:09 -0700

    fixing cheese card

--- src/components/CheeseCard/CheeseCard.jsx
+++ src/components/CheeseCard/CheeseCard.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom' 
 
 
 const CheeseCard = ({user, cheese, handleDeleteCheese}) => (
   <>
   <Card>
-    <Image src={cheese.image} wrapped ui={false} />
+    <Image src='{cheese.image}' wrapped ui={false} />
     <Card.Content>
       <Card.Header>{cheese.name ? cheese.name : cheese.category}</Card.Header>
-      <Card.Meta>{cheese.category}</Card.Meta>
+      <Card.Meta>Category: {cheese.category}</Card.Meta>
       <Card.Description>
-        {cheese.country}
-        {cheese.brand}
-        {cheese.color}
-        {cheese.mould}
-        {cheese.gasHoles}
-        {cheese.texture}
+        Country: {cheese.country}<br/>
+        Brand: {cheese.brand}<br/>
+        Color: {cheese.color}<br/>
+        Mould: {cheese.mould}<br/>
+        Gas Holes: {cheese.gasHoles}<br/>
+        Texture: {cheese.texture}<br/>
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
       <a>
         <Icon name='user' />
-        {cheese.addedBy}
-      </a>
+        {cheese.addedBy.name}
+      </a> <br/>
+      <Link 
+          to={{
+              pathname: '/edit',
+              state: {cheese}
+          }}
+      >
+        Edit Cheese
+      </Link>
+      <Button basic color='red' onClick={() => handleDeleteCheese(cheese._id)}>Delete</Button>
     </Card.Content>
   </Card>
   </>
